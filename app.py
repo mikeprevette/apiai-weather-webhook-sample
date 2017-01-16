30e596be438f95954d94cdd2963e33f4e95cb6b3
Mikeprevette <mike.prevette@gmail.com>
2017-01-16 16:42:34 -0500

    Force new York
    Test

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def makeYqlQuery(req):
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     if city is None:
-        return None
+        return "new york"
 
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
